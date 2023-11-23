d59765da9ae0ba39a7dce6e9198afae66deb5945
Max-Andre' Zils <maxandre.zils@kantan.tech>
2023-11-23 13:16:26 +0200

    fix: added some components nav and search

---
+++ tests/pom/components/navigation.component.ts
@@ -0,0 +1,13 @@
+import { type Locator, type Page } from '@playwright/test';
+
+export class NavigationComponent {
+    readonly page: Page;
+    readonly allProductsLink: Locator; 
+    
+    
+    constructor(page: Page) { 
+        this.page = page;
+        this.allProductsLink = page.getByRole('link', { name: 'All Products' }).first();
+    }
+
+}
