840069810107b5212cd6a8651f82a6cbc534ea6e
Max-Andre' Zils <maxandre.zils@kantan.tech>
2023-12-11 11:41:14 +0200

    feat: :sparkles: added linting to the repo

--- tests/pom/components/navigation.page.ts
+++ tests/pom/components/navigation.page.ts
@@ -1,13 +1,14 @@
 import { type Locator, type Page } from '@playwright/test';
 
 export class NavigationComponent {
-    readonly page: Page;
-    readonly allProductsLink: Locator; 
-    
-    
-    constructor(page: Page) { 
-        this.page = page;
-        this.allProductsLink = page.getByRole('link', { name: 'All Products' }).first();
-    }
+  readonly page: Page;
 
+  readonly allProductsLink: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.allProductsLink = page
+      .getByRole('link', { name: 'All Products' })
+      .first();
+  }
 }
