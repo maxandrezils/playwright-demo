22df3c2a441adc3f6155ca87e035442661aff41e
maxandrezils <zilsmc@gmail.com>
2024-01-22 15:11:37 +0200

    fix: :fire: removed visual comp scenarios

--- tests/spec/visualComparisons/loginScreen.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('Confirm the layout of the Login Page @visual', async ({ page }) => {
-    await page.goto('/');
-    await expect(page).toHaveScreenshot();
-    await page.close();
-});
