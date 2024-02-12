9c0fd3ae3a00433bdd16cbd88044bfa10085c09e
Max-Andre Zils <max-andre.zils@medi2data.com>
2024-02-12 10:38:34 +0200

    feat: added pom file for the site header

---
+++ tests/pom/components/header.page.ts
@@ -0,0 +1,23 @@
+import { type Locator, type Page } from '@playwright/test';
+
+export class Header {
+    readonly page: Page;
+    readonly cartLink: Locator;
+    readonly menu: Locator;
+    readonly closeMenu: Locator;
+    readonly allItems: Locator;
+    readonly about: Locator;
+    readonly logout: Locator;
+    readonly shoppingCart: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.cartLink = page.locator('#shopping_cart_container a');
+        this.menu = page.getByRole('button', { name: 'Open Menu' });
+        this.closeMenu = page.getByRole('button', { name: 'Close Menu' });
+        this.allItems = page.getByRole('link', { name: 'All Items' });
+        this.about = page.getByRole('link', { name: 'About' });
+        this.logout = page.getByRole('link', { name: 'Logout' });
+        this.shoppingCart = page.locator('#shopping_cart_container a');
+    }
+}
