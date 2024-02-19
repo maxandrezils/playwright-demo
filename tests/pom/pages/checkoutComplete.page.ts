497581ff23c3cc5b197f194cd950e3654aed6457
Max-Andre Zils <max-andre.zils@medi2data.com>
2024-02-19 09:26:57 +0200

    added a todo file for the outstanding tests

--- tests/pom/pages/checkoutComplete.page.ts
+++ tests/pom/pages/checkoutComplete.page.ts
@@ -2,10 +2,11 @@ import { Page, Locator } from '@playwright/test';
 
 export class checkoutCompletePage {
     readonly page: Page;
-    
+    readonly placeholder: Locator;
 
     constructor(page: Page) {
         this.page = page;
+        this.placeholder = page.locator('');
 
     }
 }
